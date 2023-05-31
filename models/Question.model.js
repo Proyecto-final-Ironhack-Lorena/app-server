41a2c14ecdbb11387942da5c18d715400fcfb94b
Lorena Lario <lorenalariotudela1999@gmail.com>
2023-05-31 20:28:09 +0200

    Question model terminado

---
+++ models/Question.model.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require("mongoose");
+
+const questionSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  kind: {
+    type: String,
+    enum: ["Síntoma", "¿Puedo comerlo?", "Dolor"],
+    required: true,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+});
+
+const Question = model("Question", questionSchema);
+
+module.exports = Question;
